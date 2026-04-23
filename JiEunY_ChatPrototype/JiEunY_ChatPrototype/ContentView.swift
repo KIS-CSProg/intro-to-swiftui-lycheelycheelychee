03a5f6f3290ee7d39e9186c0e6eb69b78608b7f1
lycheelycheelychee <jiyoon27@student.kis.or.kr>
2026-04-23 13:07:14 +0900

    Patience and humility are essential.

--- JiEunY_ChatPrototype/JiEunY_ChatPrototype/ContentView.swift
+++ JiEunY_ChatPrototype/JiEunY_ChatPrototype/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Patience and humility are essential.")
         }
         .padding()
     }

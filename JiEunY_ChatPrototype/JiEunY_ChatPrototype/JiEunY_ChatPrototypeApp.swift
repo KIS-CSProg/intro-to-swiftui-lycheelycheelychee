3fe31ec57d8e3dd48117232b572259ac209e9280
lycheelycheelychee <jiyoon27@student.kis.or.kr>
2026-04-23 13:03:46 +0900

    Create ChatPrototype Project

---
+++ JiEunY_ChatPrototype/JiEunY_ChatPrototype/JiEunY_ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  JiEunY_ChatPrototypeApp.swift
+//  JiEunY_ChatPrototype
+//
+//  Created by JEY on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct JiEunY_ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
